8711b558ddba1f32a9fc072eb462193443bcc630
Ivan Mincik <ivan.mincik@gmail.com>
2015-12-18 21:57:42 +0100

    vagrant: one more pan in 'tmux-dev'

--- utils/tmux-dev.sh
+++ utils/tmux-dev.sh
@@ -11,14 +11,24 @@ fi
 tmux has-session -t development
 if [ $? != 0 ]; then
     tmux new-session -s development -n servers -d
-    tmux send-keys -t development 'workon gislab-web' C-m
-    tmux send-keys -t development 'cd /vagrant/dev' C-m
-    tmux send-keys -t development 'python ./manage.py runsslserver 0.0.0.0:8000' C-m
+
     tmux split-window -v -t development
+    tmux select-pane -t 0
     tmux split-window -v -t development
-    tmux select-layout -t development main-horizontal
-    tmux send-keys -t development:0.1 'sudo tail -n 0 -f /var/log/lighttpd/access.log /var/log/lighttpd/error.log' C-m
-    tmux send-keys -t development:0.2 'sudo tail -n 0 -f /var/log/lighttpd/qgis-mapserver.log' C-m
+    tmux select-pane -t 2
+    tmux split-window -h -t development
+
+    tmux send-keys -t development:0.0 'cd /vagrant/dev' C-m
+    tmux send-keys -t development:0.0 'workon gislab-web' C-m
+    tmux send-keys -t development:0.0 'clear' C-m
+
+    tmux send-keys -t development:0.1 'cd /vagrant/dev' C-m
+    tmux send-keys -t development:0.1 'workon gislab-web' C-m
+    tmux send-keys -t development:0.1 'python ./manage.py runsslserver 0.0.0.0:8000' C-m
+
+    tmux send-keys -t development:0.2 'sudo tail -n 0 -f /var/log/lighttpd/access.log /var/log/lighttpd/error.log' C-m
+    tmux send-keys -t development:0.3 'sudo tail -n 0 -f /var/log/lighttpd/qgis-mapserver.log' C-m
+
     tmux select-window -t development:0
     tmux select-pane -t 0
 fi

ec713251e547e6a6889f98577307d201bc33531e
Marcel Dancak <dancakm@gmail.com>
2016-04-28 12:25:28 +0200

    Fixed scrollbar directive

--- clients/src/core/ui/scrollbar.dir.js
+++ clients/src/core/ui/scrollbar.dir.js
@@ -3,21 +3,17 @@
 
   angular
   .module('gl.ui')
-  .directive('glScrollbarDetector', glScrollbarDetector);
+  .directive('glScroller', glScroller);
 
-  function glScrollbarDetector($timeout) {
+
+  function glScroller() {
     return {
       scope: false,
       link: function(scope, iElem, iAttrs, ctrl) {
-        $timeout(function() {
-          if (iElem[0].scrollWidth > iElem[0].clientWidth) {
-            iElem.addClass('horizontal-overflow');
-          }
-          if (iElem[0].scrollHeight > iElem[0].clientHeight) {
-            iElem.addClass('vertical-overflow');
-          }
-        });
+        var scrollElem = iElem.parent()[0];
+        SimpleScrollbar.initEl(scrollElem);
       }
     }
   }
+
 })();

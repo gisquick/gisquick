a068e15fe247a464fc435a02447d09110f5b4b50
Marcel Dancak <dancakm@gmail.com>
2016-05-25 14:53:04 +0200

    Split 'AppLoader' controller into 'SessionController' and 'LoginController'

--- clients/src/web/js/login.ctrl.js
+++ clients/src/web/js/login.ctrl.js
@@ -3,48 +3,63 @@
 
   angular
     .module('gl.web')
-    .controller('AppLoader', AppLoader);
+    .controller('SessionController', SessionController)
+    .controller('LoginController', LoginController);
 
-  function AppLoader($scope, $timeout, $q, $mdDialog, projectProvider, gislabClient, staticResources) {
-    $scope.staticResources = staticResources;
+  /**
+   * Shared data between SessionController and LoginController
+   */
+  var PROJECT_ERROR = {
+    401: 'Authentication Required',
+    403: 'Permissions Denied'
+  };
+  var confirmed = false;
+  var heldProjectData;
+  var heldProjectUser;
+  var projectInfo = {};
+
+  /**
+   * Manages user session with the application - triggers initialization
+   * of the application or login window. It also allows to close current
+   * session and start a new one.
+   */
+  function SessionController($scope, $q, $mdDialog, projectProvider, gislabClient) {
     var projectPath = goog.uri.utils.getParamValue(location.search, "PROJECT");
     var projectStorageKey = 'gislab.project.path={0}'.format(projectPath);
-    var projectError = {
-      401: 'Authentication Required',
-      403: 'Permissions Denied'
-    };
 
-    var confirmed = false;
-    var heldProjectData;
-    var heldProjectUser;
-    var projectInfo = {};
+    $scope.sessionInitialized = false;
+
+    function showLoginScreen() {
+      $scope.sessionInitialized = false;
+
+      $mdDialog.show({
+        templateUrl: 'templates/login.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose: false,
+        hasBackdrop: false,
+        escapeToClose: false,
+        controller: 'LoginController',
+        fullscreen: true
+      });
+    }
+
+    $scope.newSession = function() {
+      var closeClinentSession = (gislabClient.userInfo.is_guest === false)?
+        gislabClient.logout() : $q.when();
+
+      closeClinentSession.then(function() {
+        localStorage.removeItem(projectStorageKey);
+        projectProvider.setProjectData(null);
+        showLoginScreen();
+      });
+    };
 
     /**
-    * Reinitialize whole application/project (by temporary reset
-    * of 'appInitialized' variable binded to ng-if directive)
-    * and launch login window
-    */
-    $scope.appInitialized = true;
-
-    function closeProject() {
-      localStorage.removeItem(projectStorageKey);
-      projectProvider.setProjectData(null);
-      $scope.appInitialized = false;
-      $timeout(function() {
-        $scope.appInitialized = true;
-      }, 50);
-      showLoginScreen();
-    }
-    
-    $scope.newSession = function() {
-      if (gislabClient.userInfo.is_guest === false) {
-        gislabClient.logout()
-          .then(closeProject);
-      } else {
-        closeProject();
-      }
-    };
-
+     * Check project data response and decides whether run main application
+     * with this project or show login screen.
+     *
+     * @param {object} project Project data
+     */
     function validateProject(project) {
       var activeProjectChanged = false;
 
@@ -76,6 +91,7 @@
             JSON.stringify(activeProject)
           );
         }
+        $scope.sessionInitialized = true;
       }
       if (project.status > 400 || !activeProject || !activeProject.loginWindowConfirmed) {
         // stop 'projectDataAvailable' event and temporary store
@@ -84,96 +100,90 @@
         heldProjectData = project;
         heldProjectUser = gislabClient.userInfo;
         projectProvider.data = null;
-        projectInfo.error = projectError[project.status];
+        projectInfo.error = PROJECT_ERROR[project.status];
         showLoginScreen();
-        return true;
+        return true; // true for stopping event dispatching
       }
       $scope.user = gislabClient.userInfo;
       heldProjectData = null;
       heldProjectUser = null;
     }
 
-    function showLoginScreen() {
-      var scope = $scope.$new();
-
-      function loadProject() {
-        return gislabClient.project(projectPath)
-          .then(
-            function(projectData) {
-              $mdDialog.hide();
-              projectProvider.setProjectData(projectData);
-            },
-            function(err) {
-              projectInfo.error = projectError[err.status_code];
-            }
-          );
-      }
-
-      scope.projectInfo = projectInfo;
-      scope.continueMode = gislabClient.userInfo.is_guest === false;
-      scope.form = {
-        username: scope.continueMode? gislabClient.userInfo.username : '',
-        password: scope.continueMode? '********' : ''
-      };
-
-      scope.switchAccount = function() {
-        scope.continueMode = false;
-        scope.form.username = '';
-        scope.form.password = '';
-      };
-
-      scope.loginOrContinue = function() {
-        confirmed = true;
-        scope.error = '';
-        if (scope.continueMode) {
-          if (heldProjectData && heldProjectUser.username === gislabClient.userInfo.username) {
-            $mdDialog.hide();
-            projectProvider.setProjectData(heldProjectData);
-
-          } else {
-            loadProject();
-          }
-        } else {
-          gislabClient.login(scope.form.username, scope.form.password)
-            .then(function(userData) {
-              loadProject();
-            }, function(err) {
-              scope.error = 'Authentication Failed';
-            });
-        }
-      };
-
-      if (projectInfo && projectInfo.authentication === 'all') {
-        scope.continueAsGuest = function() {
-          scope.error = '';
-          confirmed = true;
-          if (heldProjectData && heldProjectUser.is_guest && gislabClient.userInfo.is_guest) {
-            $mdDialog.hide();
-            projectProvider.setProjectData(heldProjectData);
-          } else {
-            gislabClient.login("guest")
-              .then(function(userData) {
-                loadProject();
-              })
-          }
-        };
-      }
-
-      $mdDialog.show({
-        templateUrl: 'templates/login.html',
-        parent: angular.element(document.body),
-        clickOutsideToClose: false,
-        hasBackdrop: false,
-        escapeToClose: false,
-        scope: scope,
-        fullscreen: true
-      });
-    }
-
     projectProvider.on('projectDataAvailable', validateProject);
     if (projectProvider.data) {
       validateProject(projectProvider.data);
     }
+  }
+
+  /**
+   * Controller for login dialog window
+   */
+  function LoginController($scope, $mdDialog, projectProvider, gislabClient, staticResources) {
+    $scope.staticResources = staticResources;
+    var projectPath = goog.uri.utils.getParamValue(location.search, "PROJECT");
+
+    $scope.projectInfo = projectInfo;
+    $scope.continueMode = gislabClient.userInfo.is_guest === false;
+    $scope.form = {
+      username: $scope.continueMode? gislabClient.userInfo.username : '',
+      password: $scope.continueMode? '********' : ''
+    };
+
+    function loadProject() {
+      return gislabClient.project(projectPath)
+        .then(
+          function(projectData) {
+            $mdDialog.hide();
+            projectProvider.setProjectData(projectData);
+          },
+          function(err) {
+            projectInfo.error = PROJECT_ERROR[err.status_code];
+          }
+        );
+    }
+
+    $scope.loginOrContinue = function() {
+      confirmed = true;
+      $scope.error = '';
+      if ($scope.continueMode) {
+        if (heldProjectData && heldProjectUser.username === gislabClient.userInfo.username) {
+          $mdDialog.hide();
+          projectProvider.setProjectData(heldProjectData);
+
+        } else {
+          loadProject();
+        }
+      } else {
+        gislabClient.login($scope.form.username, $scope.form.password)
+          .then(function(userData) {
+            loadProject();
+          }, function(err) {
+            $scope.error = 'Authentication Failed';
+          });
+      }
+    };
+
+    if (projectInfo && projectInfo.authentication === 'all') {
+      $scope.continueAsGuest = function() {
+        $scope.error = '';
+        confirmed = true;
+        if (heldProjectData && heldProjectUser.is_guest && gislabClient.userInfo.is_guest) {
+          $mdDialog.hide();
+          projectProvider.setProjectData(heldProjectData);
+        } else {
+          gislabClient.login("guest")
+            .then(function(userData) {
+              loadProject();
+            })
+        }
+      };
+    }
+
+    $scope.switchAccount = function() {
+      $scope.continueMode = false;
+      $scope.form.username = '';
+      $scope.form.password = '';
+    };
   }
 
 })();

81500708e8bd8250512cbc198c3fcf2d72ebd144
Marcel Dancak <dancakm@gmail.com>
2017-02-28 15:34:58 +0100

    Updated development files of mobile client

--- clients/tasks/mobile.js
+++ clients/tasks/mobile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-
+var merge = require('merge-stream');
 
 var TARGET = './mobile/cordova-app/www/';
 
@@ -14,8 +14,10 @@ gulp.task('uglify', function() {
 
   return gulp.src([
     'src/core/**/*.module.js',
+    'src/web/map/js/**/*.module.js',
     'src/mobile/js/**/*.module.js',
     'src/core/**/*.js',
+    'src/web/map/js/**/*.js',
     'src/mobile/js/**/*.js'
   ])
     .pipe(ngAnnotate({ add: true }))
@@ -29,7 +31,10 @@ gulp.task('uglify', function() {
  * Copy html templates into target directory
  */
 gulp.task('templates', function() {
-  return gulp.src('src/mobile/templates/**/*.html')
+  return gulp.src([
+      'src/web/map/templates/**/*.html',
+      'src/mobile/templates/**/*.html'
+    ])
     .pipe(gulp.dest(TARGET + 'templates/'));
 });
 
@@ -39,22 +44,30 @@ gulp.task('templates', function() {
  * into the target styles directory
  */
 gulp.task('csss', function() {
-  var merge = require('merge-stream');
   var minifyCss = require('gulp-minify-css');
 
   return merge(
     gulp.src([
       'node_modules/openlayers/dist/ol.css',
-      'node_modules/gislab-mobile/node_modules/onsenui/css/onsenui.css',
-      'node_modules/gislab-mobile/node_modules/onsenui/css/onsen-css-components.css',
+      'node_modules/gislab-web/node_modules/angular-material/angular-material.css',
+      'src/web/styles/*.css',
+      'src/web/map/styles/ui.css', // load ui first
+      'src/web/map/styles/**/*.css',
       'src/mobile/styles/**/*.css'
     ])
       .pipe(minifyCss())
       .pipe(concat('styles.min.css'))
       .pipe(gulp.dest(TARGET + 'styles')),
 
-    gulp.src('src/mobile/styles/*.svg')
-      .pipe(gulp.dest(TARGET + 'styles'))
+    gulp.src('src/web/styles/*.svg')
+      .pipe(gulp.dest(TARGET + 'styles')),
+
+    gulp.src('src/web/map/styles/*.png')
+      .pipe(gulp.dest(TARGET + 'styles/')),
+
+    gulp.src('src/web/styles/fonts/*')
+      .pipe(gulp.dest(TARGET + 'styles/fonts'))
+
   );
 });
 
@@ -62,32 +75,36 @@ gulp.task('csss', function() {
 /**
  * Build all JavaScript libraries into single minified file
  */
-gulp.task('deps', ['build-ol3'], function() {
+// gulp.task('deps', ['build-ol3'], function() {
+gulp.task('deps', function() {
   var uglify = require('gulp-uglify');
   var gulpif = require('gulp-if');
 
-  return gulp.src([
-    'node_modules/crypto-js/core.js',
-    'node_modules/crypto-js/md5.js',
-    'node_modules/proj4/dist/proj4.js',
-    'node_modules/openlayers/build/ol.min.js',
+  return merge(
+    // copy compiled ol3+deps
+    // gulp.src('node_modules/openlayers/build/ol3-deps.min.js')
+    gulp.src('node_modules/openlayers/build/ol3-deps.debug.js')
+      .pipe(concat('ol3-deps.min.js'))
+      .pipe(gulp.dest(TARGET + 'js/')),
 
-    'node_modules/gislab-mobile/node_modules/angular/angular.min.js',
-    'node_modules/gislab-mobile/node_modules/angular-animate/angular-animate.min.js',
-    'node_modules/gislab-mobile/node_modules/onsenui/js/onsenui.min.js',
-    'node_modules/gislab-mobile/node_modules/onsenui/js/angular-onsenui.min.js',
-    'node_modules/gislab-mobile/node_modules/ngstorage/ngStorage.min.js'
-  ])
-    .pipe(
-      gulpif(
-        function (file) {
-          return !file.path.endsWith("min.js")
-        },
-        uglify()
+    gulp.src([
+      'node_modules/gislab-web/node_modules/angular/angular.min.js',
+      'node_modules/gislab-web/node_modules/angular-animate/angular-animate.min.js',
+      'node_modules/gislab-web/node_modules/angular-aria/angular-aria.min.js',
+      'node_modules/gislab-web/node_modules/angular-material/angular-material.gislab.js',
+      'node_modules/gislab-mobile/node_modules/ngstorage/ngStorage.min.js'
+    ])
+      .pipe(
+        gulpif(
+          function (file) {
+            return !file.path.endsWith("min.js")
+          },
+          uglify()
+        )
       )
-    )
-    .pipe(concat('deps.min.js'))
-    .pipe(gulp.dest(TARGET + 'js/'));
+      .pipe(concat('deps.min.js'))
+      .pipe(gulp.dest(TARGET + 'js/'))
+  );
 });
 
 
@@ -95,7 +112,7 @@ gulp.task('deps', ['build-ol3'], function() {
  * Copy index.html file into target directory
  */
 gulp.task('index-page', function() {
-  return gulp.src('src/mobile/index.html')
+  return gulp.src('mobile/cordova-index.html')
     .pipe(concat('index.html'))
     .pipe(gulp.dest(TARGET));
 });
@@ -110,18 +127,26 @@ gulp.task('serve', function() {
     port: 8200,
     root: [
       '.',
-      'src/',
+      'mobile/dev/',
       'src/mobile/',
-      'mobile/dev',
+      'src/',
+      'src/web/map/',
+      'node_modules/gislab-web/',
       'node_modules/gislab-mobile/'
     ],
     src: [
       'src/core/**/*.js',
-      'src/mobile/**/*.js',
-      'src/mobile/**/*.html',
-      'mobile/dev/index.html'
+      'src/web/map/**/*.js',
+      'src/web/map/**/*.html',
+      'src/mobile/js/**/*.js',
+      'src/mobile/templates/**/*.html',
+      'src/mobile/index.html'
     ],
-    css: 'src/mobile/styles/**/*.css'
+    css: [
+      'src/web/styles/**/*.css',
+      'src/web/map/styles/**/*.css',
+      'src/mobile/styles/*.css'
+    ]
   };
   gulp.start('serve');
 });

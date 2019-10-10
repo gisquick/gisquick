e064c54b9236e865e038a285faf43605a8d5d65b
Marcel Dancak <dancakm@gmail.com>
2019-10-10 08:54:39 +0200

    Upgraded django to fix build

--- server/webgis/conf/project_template/project_name/settings.py
+++ server/webgis/conf/project_template/project_name/settings.py
@@ -89,6 +89,28 @@ INSTALLED_APPS = (
     'webgis.mobile'
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                #'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = '{{ project_name }}.urls'
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 

7150d4b4a19a698c8c80b7759be24b2526387c50
Marcel Dancak <dancakm@gmail.com>
2021-05-14 10:30:01 +0200

    web: update of js libraries, custom svg sprite plugin

--- clients/gisquick-web/vue.config.js
+++ clients/gisquick-web/vue.config.js
@@ -1,43 +1,19 @@
 const CompressionPlugin = require('compression-webpack-plugin')
+const SpritePlugin = require('./svg-sprite.js')
 
 module.exports = {
+  lintOnSave: 'warning',
   publicPath: process.env.NODE_ENV === 'production' ? '/map/' : '/',
   assetsDir: 'static',
   css: {
     extract: process.env.NODE_ENV === 'production' && process.env.CSS_EXTRACT !== 'False'
   },
   configureWebpack: {
-    plugins: [new CompressionPlugin()]
+    plugins: [
+      new CompressionPlugin(),
+      new SpritePlugin({ path: './icons' })
+    ]
   },
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-
-    config.module
-      .rule('svg')
-      .oneOf('sprite')
-      .test(/icons\/.*\.svg$/)
-      .use('babel')
-      .loader('babel-loader')
-      .end()
-      .use('svg-sprite')
-      .loader('svg-sprite-loader')
-      .end()
-      .use('svgo')
-      .loader('svgo-loader')
-      .end()
-      .end()
-
-      .oneOf('other')
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'static/img/[name].[hash:8].[ext]'
-      })
-      .end()
-      .end()
-  },
-
   devServer: {
     proxy: {
       '^/api': {

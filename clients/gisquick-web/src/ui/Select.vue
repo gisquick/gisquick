b6927bc2b16e073b8bc701c6a3d782ee41767df6
Marcel Dancak <dancakm@gmail.com>
2022-03-14 08:54:10 +0100

    web: updated Select UI component (multiple selection model)

--- clients/gisquick-web/src/ui/Select.vue
+++ clients/gisquick-web/src/ui/Select.vue
@@ -6,98 +6,109 @@
     :label="label"
     :color="color"
   >
-  <div
-    ref="select"
-    role="button"
-    class="input"
-    :class="{open}"
-    :disabled="disabled"
-    aria-haspopup="true"
-    :aria-disabled="disabled"
-    :aria-expanded="open ? 'true' : 'false'"
-    :aria-owns="listId"
-    :tabindex="disabled ? -1 : 0"
-    @focus="onFocus"
-    @blur="onBlur"
-    @click="toggle"
-  >
-    <slot
-      name="selection"
-      v-if="selected && selected.item"
-      v-bind="selected"
+    <div
+      ref="select"
+      role="button"
+      class="input"
+      :class="{open}"
+      :disabled="disabled"
+      aria-haspopup="true"
+      :aria-disabled="disabled"
+      :aria-expanded="open ? 'true' : 'false'"
+      :aria-owns="listId"
+      :tabindex="disabled ? -1 : 0"
+      @focus="onFocus"
+      @blur="onBlur"
+      @click="toggle"
     >
-      <span class="value f-grow">{{ simpleItems ? selected.item : selected.item[itemText] }}</span>
-    </slot>
-    <span v-else class="placeholder f-grow">{{ placeholder }}</span>
-    <svg
-      class="toggle"
-      width="11"
-      height="11"
-      viewBox="0 0 10 10"
-      @click.stop="toggle"
-    >
-      <path d="M 1,3 L 5,7 L 9,3"/>
-    </svg>
-    <document-listener
-      v-if="focused && !open && !disabled"
-      @keydown.space.prevent="openPopup"
-      @keydown.up.prevent="select(selected.index - 1)"
-      @keydown.down.prevent="select(selected.index + 1)"
-      @keydown="keyHandler"
-    />
-    <popup-content
-      backhandler
-      transition="slide-y"
-      :open.sync="open"
-      :bounds="bounds"
-      :align="align"
-      :popup-class="contentClass"
-      :popup-style="popupStyle"
-      @click:out="popupClick"
-      @click:in="closePopup"
-      @keydown.esc="closePopup"
-      @keydown.tab.prevent="closePopup"
-      @keydown.up.prevent="highlight(highlightIndex - 1)"
-      @keydown.down.prevent="highlight(highlightIndex + 1)"
-      @keydown.enter="onEnter"
-      @keydown.space.prevent=""
-      @keydown="keyHandler"
-      @opened="highlight(selected.index)"
-    >
-      <div
-        class="popup-content popup-list"
-        role="listbox"
-        tabindex="-1"
-        :id="listId"
-        :style="colorVars"
+      <slot
+        v-if="selection.items.size"
+        name="selection"
+        v-bind="selection"
+        :toggle="selectItem"
       >
-        <template v-for="(item, index) in items">
-          <div
-            :key="index"
-            ref="item"
-            tabindex="0"
-            role="option"
-            class="item"
-            :class="{
-              highlighted: highlightIndex === index,
-              selected: selected.item === item
-            }"
-            @click="selectItem(item)"
-            @mouseover="highlightIndex = index"
-          >
-            <slot
-              name="item"
-              :item="item"
-              :index="index"
-              :selected="selected.item === item"
+        <span class="value f-grow" v-text="selection.text"/>
+      </slot>
+      <span v-else class="placeholder f-grow" v-text="placeholder"/>
+      <svg
+        class="toggle"
+        width="11"
+        height="11"
+        viewBox="0 0 10 10"
+        @click.stop="toggle"
+      >
+        <path d="M 1,3 L 5,7 L 9,3"/>
+      </svg>
+      <document-listener
+        v-if="focused && !open && !disabled"
+        @keydown.space.prevent="openPopup"
+        @keydown.up.prevent="select(selection.index - 1)"
+        @keydown.down.prevent="select(selection.index + 1)"
+        @keydown="keyHandler"
+      />
+      <popup-content
+        backhandler
+        transition="slide-y"
+        :open.sync="open"
+        :bounds="bounds"
+        :align="align"
+        :popup-class="contentClass"
+        :popup-style="popupStyle"
+        @click:out="clickOut"
+        @keydown.esc="closePopup"
+        @keydown.tab.prevent="closePopup"
+        @keydown.up.prevent="keyboardHighlightPrev"
+        @keydown.down.prevent="keyboardHighlightNext"
+        @keydown.enter="selectHighlighted"
+        @keydown.space.prevent="selectHighlighted"
+        @keydown="keyHandler"
+        @opened="onOpened"
+      >
+        <div
+          class="popup-content popup-list"
+          role="listbox"
+          tabindex="-1"
+          :id="listId"
+          :style="colorVars"
+          @mousemove="keyboardLock = false"
+        >
+          <template v-for="(item, index) in normItems">
+            <template v-if="item.separator">
+              <div v-if="item.text" :key="index" class="separator f-row-ac">
+                <hr class="f-grow"/>
+                <span class="separator mx-2" v-text="item.text"/>
+                <hr class="f-grow"/>
+              </div>
+              <hr v-else :key="index"/>
+            </template>
+            <div
+              v-else
+              :key="index"
+              ref="item"
+              tabindex="0"
+              role="option"
+              class="item"
+              :class="{
+                highlighted: highlightIndex === index,
+                selected: selection.items.has(item)
+              }"
+              :aria-disabled="item.disabled"
+              @click="selectItem(item)"
+              @mouseover="onMouseOver(index)"
             >
-              <span v-text="simpleItems ? item : item[itemText]" class="f-grow m-2"/>
-            </slot>
-          </div>
-        </template>
-      </div>
-    </popup-content>
-  </div>
+              <slot
+                name="item"
+                :item="item"
+                :index="index"
+                :selected="selection.items.has(item)"
+              >
+                <span v-text="item[itemText]" class="f-grow m-2"/>
+              </slot>
+            </div>
+          </template>
+        </div>
+      </popup-content>
+    </div>
   </input-field>
 </template>
 
@@ -139,7 +150,8 @@ export default {
     },
     label: String,
     placeholder: String,
-    value: {}
+    value: {},
+    multiple: Boolean
   },
   data () {
     return {
@@ -148,7 +160,8 @@ export default {
       highlightIndex: -1,
       focused: false,
       search: '',
-      listId: null
+      listId: null,
+      keyboardLock: false
     }
   },
   computed: {
@@ -158,13 +171,24 @@ export default {
     colorVars () {
       return colorVars(this.color)
     },
-    selected () {
-      const index = this.items.findIndex(item => this._itemValue(item) === this.value)
-      const item = this.items[index]
-      return { index, item }
-    },
     simpleItems () {
-      return this.items.some(i => i[this.itemText] === undefined)
+      return this.items.some(i => !Object.prototype.hasOwnProperty.call(i, this.itemText))
+    },
+    normItems () {
+      return this.simpleItems ? this.items.map(v => ({ [this.itemText]: v?.toString() || '', [this.itemValue]: v })) : this.items
+    },
+    selection () {
+      if (this.multiple) {
+        const items = this.value?.map(v => this.normItems.find(i => i[this.itemValue] === v)) || []
+        const texts = items.map(i => i[this.itemText])
+        return { items: new Set(items), text: texts.join(', ') }
+      }
+      const index = this.normItems.findIndex(i => i[this.itemValue] === this.value)
+      if (index !== -1) {
+        const item = this.normItems[index]
+        return { index, item, items: new Set().add(item), text: item[this.itemText] }
+      }
+      return { items: new Set() }
     }
   },
   watch: {
@@ -176,8 +200,11 @@ export default {
     this.listId = `select-list-${counter++}`
   },
   methods: {
-    _itemValue (item) {
-      return !this.itemValue || this.simpleItems ? item : item[this.itemValue]
+    focusComponent () {
+      if (!this.disabled) {
+        // this.$el.focus()
+        this.$refs.select.focus()
+      }
     },
     onFocus () {
       if (!this.disabled) {
@@ -198,67 +225,116 @@ export default {
     closePopup () {
       this.open = false
     },
-    toggle () {
-      if (!this.disabled) {
-       this.open ? this.closePopup() : this.openPopup()
+    onOpened () {
+      this.highlightIndex = 0
+      if (!this.multiple) {
+        this.highlight(this.selection.index)
+      } else {
+        // this.highlight(0)
       }
     },
-    popupClick (e) {
+    clickOut (e) {
       if (!this.$el.contains(e.target)) {
         this.closePopup()
         this.focused = false
       }
     },
-    highlight (index) {
-      this.highlightIndex = Math.max(0, Math.min(this.items.length - 1, index))
-      const el = this.$refs.item && this.$refs.item[this.highlightIndex]
-      el && el.scrollIntoView({ block: 'nearest' })
-    },
-    onEnter () {
-      if (this.highlightIndex !== -1) {
-        this.select(this.highlightIndex)
-        this.closePopup()
+    toggle () {
+      if (!this.disabled) {
+       this.open ? this.closePopup() : this.openPopup()
       }
     },
-    focusComponent () {
-      if (!this.disabled) {
-        // this.$el.focus()
-        this.$refs.select.focus()
+    highlight (index) {
+      this.highlightIndex = Number.isInteger(index) ? Math.max(0, Math.min(this.items.length - 1, index)) : 0
+      const el = this.$refs.item?.[this.highlightIndex]
+      el && el.scrollIntoView({ block: 'nearest' })
+    },
+    isItemSelectable (item) {
+      return !item.disabled && !item.separator
+    },
+    findIndex (predicate, start = 0, end) {
+      end = end || this.items.length
+      for (let i = start; i < end; i++) {
+        if (predicate(this.normItems[i])) {
+          return i
+        }
+      }
+      return -1
+    },
+    keyboardHighlightNext () {
+      this.keyboardLock = true
+      const index = this.findIndex(this.isItemSelectable, this.highlightIndex + 1)
+      if (index !== -1) {
+        this.highlight(index)
+      }
+    },
+    keyboardHighlightPrev () {
+      this.keyboardLock = true
+      for (let i = this.highlightIndex - 1; i >= 0; i--) {
+        if (this.isItemSelectable(this.normItems[i])) {
+          this.highlight(i)
+          break
+        }
+      }
+    },
+    selectHighlighted (e) {
+      if (e?.code === 'Space' && this.search) {
+        return
+      }
+      const item = this.normItems[this.highlightIndex]
+      if (item) {
+        this.selectItem(item)
       }
     },
     select (index) {
-      const item = this.items[index]
+      const item = this.normItems[index]
       item && this.selectItem(item)
     },
     selectItem (item) {
-      this.$emit('input', this._itemValue(item))
+      let value = item[this.itemValue]
+      if (this.multiple) {
+        if (this.value?.includes(value)) {
+          value = this.value.filter(v => v !== value)
+        } else {
+          value = this.value?.concat(value) || [value]
+        }
+      } else {
+        this.closePopup()
+      }
+      this.$emit('input', value)
       this.focusComponent()
+    },
+    onMouseOver (index) {
+      if (!this.keyboardLock) {
+        this.highlightIndex = index
+      }
     },
     _clearSearch: debounce(function () {
       this.search = ''
-    }, 400),
+    }, 450),
+    _search: debounce(function () {
+      const action = this.open ? this.highlight : this.select
+      const currentIndex = this.open ? this.highlightIndex : this.selection?.index || 0
+
+      const re = new RegExp('^' + escapeRegExp(removeDiacritics(this.search)), 'i')
+      const testItem = item => this.isItemSelectable(item) && re.test(removeDiacritics(item[this.itemText]))
+      const startIndex = currentIndex !== -1 ? currentIndex + 1 : 0
+      let index = this.findIndex(testItem, startIndex)
+      if (index === -1 && startIndex > 0) { // search again from the beginning
+        index = this.findIndex(testItem)
+      }
+      if (index !== -1) {
+        action(index)
+      }
+    }, 100),
     keyHandler (e) {
+      if (!this.open && this.multiple) {
+        return
+      }
       if (e.key.length === 1) {
         this.search += e.key
+        this._search()
         this._clearSearch()
-        const action = this.open ? this.highlight : this.select
-        const currentIndex = this.open ? this.highlightIndex : this.selected.index
-        const re = new RegExp('^' + escapeRegExp(removeDiacritics(this.search)), 'i')
-        const testItem = item => {
-          const value = this._itemValue(item)?.toString()
-          return re.test(removeDiacritics(value))
-        }
-        if (this.search.length === 1 && currentIndex !== -1) {
-          const next = this.items[currentIndex + 1]
-          if (next && testItem(this.items[currentIndex]) && testItem(next)) {
-            action(currentIndex + 1)
-            return
-          }
-        }
-        const index = this.items.findIndex(testItem)
-        if (index !== -1) {
-         action(index)
-        }
       }
     }
   }

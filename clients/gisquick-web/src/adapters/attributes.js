c2073c2c8ef839fc184372e28e0b2678bcef52dd
Marcel Dancak <dancakm@gmail.com>
2023-01-04 16:04:45 +0100

    updated processing of ValueMap config for new api

--- clients/gisquick-web/src/adapters/attributes.js
+++ clients/gisquick-web/src/adapters/attributes.js
@@ -1,4 +1,10 @@
 const ConversionFunctions = {
+  // new API
+  int: v => parseInt(v),
+  uint: v => parseInt(v),
+  bool: v => Boolean(v),
+  float: v => parseFloat(v),
+  // old API
   INTEGER: v => parseInt(v),
   DOUBLE: v => parseFloat(v),
   BOOL: v => Boolean(v)
